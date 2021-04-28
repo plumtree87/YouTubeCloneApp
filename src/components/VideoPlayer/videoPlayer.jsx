2ac6438468991c2a748b29f312738ede1f3ada29
plumtree87 <plumtree87@protonmail.com>
2021-04-28 21:15:17 +0500

    saving changes before pull

--- src/components/VideoPlayer/videoPlayer.jsx
+++ src/components/VideoPlayer/videoPlayer.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import card from 'react-bootstrap/card';
+import Accordion from 'react-bootstrap/Accordion'
+import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
 
 const VideoPlayer = ({ video }) => {
   if (!video) {
@@ -9,6 +12,7 @@ const VideoPlayer = ({ video }) => {
   }
 
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  const decoratedOnClick = useAccordionToggle(eventKey, onClick);
   console.log(typeof video);
   return (
       <center>
@@ -18,8 +22,20 @@ const VideoPlayer = ({ video }) => {
       </div>
       <div>
         <h4 id='selectedVidTitle'>{video.snippet.title}</h4>
-        <p id='selectedVidDesc'>{video.snippet.description}</p>
-      </div>
+        <Accordion defaultActiveKey="0">
+        <card>
+        <card.header>
+        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+        Video Description
+      </Accordion.Toggle>
+        </card.header>
+        <Accordion.Collapse eventKey="0">
+      <Card.Body> <p id='selectedVidDesc'>{video.snippet.description}</p></Card.Body>
+    </Accordion.Collapse>
+        </card>
+        </Accordion>
+    </div>
+       
     </div>
     </center>
     
